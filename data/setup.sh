d9ab311ea92a730980365092f731605e8150fd27
Jack Skinner <sydnerdrage@gmail.com>
2015-02-05 04:13:21 +1100

    Updated minor deployment tweaks for apache and setup script.

--- data/setup.sh
+++ data/setup.sh
@@ -10,11 +10,13 @@ rm -rf ./*
 git clone https://github.com/sydnerdrage/joes-bacon-emporium.git .
 chown www-data:www-data . -R
 cat data/scaffold.sql | mysql -u root -pphunconf
-sed -i ’s/__HOST__/localhost/g’ data/apache.conf
-sed -i ’s/__USER__/root/g’ data/apache.conf
-sed -i ’s/__PASS__/phunconf/g’ data/apache.conf
-sed -i ’s/__DB__/bacon/g’ data/apache.conf
+sed -i 's/__HOST__/localhost/g' data/apache.conf
+sed -i 's/__USER__/root/g' data/apache.conf
+sed -i 's/__PASS__/phunconf/g' data/apache.conf
+sed -i 's/__DB__/bacon/g' data/apache.conf
 a2dissite 000-default
-cp data/apache2.com /etc/apache2/sites-available/001-bacon.conf
+cp data/apache.conf /etc/apache2/sites-available/001-bacon.conf
 a2ensite 001-bacon
+
+service apache2 restart
 
